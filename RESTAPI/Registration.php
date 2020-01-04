f73e7529c92b7e232648ea4172e5b6a4f5cdcd20
Parin Patel <pparin8@Gmail.com>
2020-01-05 00:00:16 +0530

    Register.php file added with error handling

--- RESTAPI/Registration.php
+++ RESTAPI/Registration.php
@@ -6,7 +6,13 @@
     $fname = $_POST['fname'];
     $lname = $_POST['lname'];
 
-    $query =  "Insert into profiles values ('2', '$fname', '$lname', '$email', '$password')";
-    $result = mysqli_query($link, $query);
-
+    $query =  "Insert into profiles (fname, lname, email, password) values ('$fname', '$lname', '$email', '$password')";
+    if(mysqli_query($link, $query)){
+        $validmessage = array("responseCode"=>201, "responseDescription"=>"User Created");
+    }else{
+        $validmessage = array("errorCode"=>403, "errorDescription"=>"The registration for this user is already Done.");
+    }
+    echo json_encode($validmessage);
+    
+    
 ?>
